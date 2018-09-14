054f8866447320a6501ad1c4a6936f70ed0e928f
Terrin Day <terrinday@gmail.com>
2018-09-14 19:29:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element)
 
 function destructivelyAddElementToBeginningOfArray(array,element)
 {
-  unshift.
+  array.unshift(element)
   return [element]
   
 }
