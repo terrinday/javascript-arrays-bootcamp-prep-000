79d9d19fc6cb39982258b03f29a108adc005350d
Terrin Day <terrinday@gmail.com>
2018-09-14 19:33:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element)
 {
-  return [array, ...element];
+  return [element, array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element)
